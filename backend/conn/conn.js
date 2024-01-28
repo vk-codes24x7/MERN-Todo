c4e3c993006ed04365f93c04d9db9cafbb578558
vk-codes24x7 <vinayakrishna1001@gmail.com>
2024-01-27 20:25:01 -0500

    added the dotenv library to secure the db connection string

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const conn = async (req, res) => {
   try {
-    await mongoose
-      .connect(
-        "mongodb+srv://vinayakrishna1001:ThisIsVinaya%409904@cluster0.w8yrg3z.mongodb.net/"
-      )
-      .then(() => {
-        console.log("The Database is connected successfully!");
-      });
+    await mongoose.connect(process.env.DB_URI).then(() => {
+      console.log("The Database is connected successfully!");
+    });
   } catch (error) {
     res.status(400).json({
       message: "Error connecting to database",
